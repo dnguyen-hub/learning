5f1eeae7af5183a4c5f9ed9458d8df692b474618
dnguyen-hub <duy.nguyen.2005.college@gmail.com>
2024-09-13 19:14:55 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,9 @@
-{ stdenv, cmake, hello}:
+{ stdenv, cmake}:
 
 
 stdenv.mkDerivation  {
   pname = "hello_lib";
   version = "0.1.0";
-  src = ./app;
+  src = ./.;
   nativeBuildInputs = [ cmake ];
-  buildInputs = [ hello ];
 }
