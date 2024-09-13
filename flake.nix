d5f972fabf0c7fdeaf443ffac23b20126241908c
dnguyen-hub <duy.nguyen.2005.college@gmail.com>
2024-09-13 19:14:02 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
   let
     hello_lib_overlay = final: prev: {
         hello = final.callPackage ./hellolib.nix { };
-        hello_lib = final.callPackage ./default.nix { };
+  
     };
     my_overlays = [hello_lib_overlay];
     pkgs = import nixpkgs {
